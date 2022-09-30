15863e3643fa0c2c2360d96ccc56563b72a74291
Harish <harishjegan30@gmail.com>
2022-09-30 09:08:10 +0530

    Created project structure

---
+++ src/main/java/com/lamda/service/Lamda_Web_service/LamdaWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lamda.service.Lamda_Web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamdaWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LamdaWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lamda/service/Lamda_Web_service/LamdaWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamda.service.Lamda_Web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamdaWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
