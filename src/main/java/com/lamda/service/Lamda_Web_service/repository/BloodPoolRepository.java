d0bbe8c8f9ded9abb2e8aaf6a7b318680dc2ebca
Harish <harishjegan30@gmail.com>
2022-10-16 19:57:33 +0530

    Deployed new version to AWS

--- src/main/java/com/lamda/service/Lamda_Web_service/repository/BloodPoolRepository.java
+++ src/main/java/com/lamda/service/Lamda_Web_service/repository/BloodPoolRepository.java
@@ -5,6 +5,6 @@ import com.lamda.service.Lamda_Web_service.model.BloodPool;
 import java.util.List;
 
 public interface BloodPoolRepository extends JpaRepository<BloodPool, Long> {
-      List<BloodPool>findByBloodGroup(String group);
-      List<BloodPool>findByBankId(Long blood_bank_id);
+      List<BloodPool>findByBloodGroup(String group); // used to find blood banks that have a given blood group
+      List<BloodPool>findByBankId(Long blood_bank_id); // used to find donations made to a blood bank
 }
